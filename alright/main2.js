3503d056fc54c19075671b565f101f68e7d41d43
Araksiaa <araksmkhitaryan@gmail.com>
2022-11-11 07:57:49 -0800

    first commit

---
+++ alright/main2.js
@@ -0,0 +1,21 @@
+//առաջադրանք 3-while-ով
+
+
+let i="*";
+while (i.length<6)  {
+    console.log(i);
+    i=i+"*";
+}
+
+
+///առաջադրանք 3-for-ով
+
+/*
+let i=5
+let y=" ";
+let v='*';
+for (let z = 0; z < i; z++) {
+  y += v;
+  console.log(y);
+}
+*/
